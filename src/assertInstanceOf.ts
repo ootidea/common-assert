745e7fd0f412e8eee8bf6fd87f9993c4f6d5932a
ootidea <ootidea@gmail.com>
2024-11-18 07:32:17 +0900

    docs: write assertInstanceOf JSDoc

--- src/assertInstanceOf.ts
+++ src/assertInstanceOf.ts
@@ -1,6 +1,18 @@
 import { AssertionError } from './AssertionError'
 import { assertTypeEquality } from './assertTypeEquality'
 
+/**
+ * Asserts that the value is an instance of the given class.
+ * @param value - The value to check
+ * @param ctor - The constructor function
+ * @throws AssertionError if the value is not an instance of the given class
+ * @example
+ * const value: unknown = new Blob()
+ * assertInstanceOf(value, Blob) // succeeds
+ * console.log(value.size) // value is narrowed to Blob
+ * @example
+ * assertInstanceOf(null, URL) // throws AssertionError: The null is not an instance of URL
+ */
 export function assertInstanceOf<T extends abstract new (..._: any) => any>(
   value: unknown,
   ctor: T,
