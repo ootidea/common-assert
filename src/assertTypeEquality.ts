8c92efad01ca9472d84027a2b3b71996e68635d5
ootidea <ootidea@gmail.com>
2024-11-18 08:53:09 +0900

    docs: write assertTypeEquality JSDoc

--- src/assertTypeEquality.ts
+++ src/assertTypeEquality.ts
@@ -1,4 +1,14 @@
-export function assertTypeEquality<T, U>(..._: Equals<T, U> extends true ? [] : [error: 'Type does not match', T, U]) {}
+/**
+ * Asserts that two types are strictly equal.
+ * This function operates during type checking, not at runtime.
+ * It results in a type error when the assertion fails.
+ * It does nothing at runtime.
+ * @example
+ * assertTypeEquality<Uppercase<'a'>, 'A'>() // succeeds
+ * @example
+ * assertTypeEquality<any, never>() // fails as a type error
+ */
+export function assertTypeEquality<T, U>(..._: Equals<T, U> extends true ? [] : [error: 'Type does not match']) {}
 
 type Equals<T, U> = (<R>() => R extends T ? 1 : 2) extends <R>() => R extends U ? 1 : 2 ? true : false
 
@@ -7,11 +17,12 @@ if (import.meta.vitest) {
     it('has no type error when two types are equal', () => {
       assertTypeEquality<number, number>()
       assertTypeEquality<boolean, true | false>()
+      assertTypeEquality<Uppercase<'a'>, 'A'>()
     })
 
     it('has type error when two types are not equal', () => {
       // @ts-expect-error
-      assertTypeEquality<string, number>()
+      assertTypeEquality<string, string & {}>()
       // @ts-expect-error
       assertTypeEquality<any, unknown>()
       // @ts-expect-error
